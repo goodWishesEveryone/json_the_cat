e8a6dfd33a88d0294646958d92e72a7ce2ea660b
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-02-10 19:42:13 -0500

    Add file breedFetcher.js to implement Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,21 @@
-const fetchBreedDescription = function(breedName, callback) {
+const request = require("request");
 
+const fetchBreedDescription = function (breedName, callback) {
+  const searchURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(searchURL, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      const data = JSON.parse(body);
+
+      if (data.length === 0) {
+        callback("Breed not found", null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
 };
 
 module.exports = { fetchBreedDescription };
